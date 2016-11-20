8e012a7536e8ed25bc3778c523b69963c689563c
Ricky Hussmann <ricky.hussmann@gmail.com>
2016-11-20 13:44:03 -0500

    Adding Restify

---
+++ index.js
@@ -0,0 +1,14 @@
+const restify = require('restify');
+
+const posts = require('./posts');
+
+var server = restify.createServer({
+  name: 'instagram-photo-picker-api',
+});
+
+server.get('/posts', (req, res, next) => {
+  res.send(200, posts);
+  return next();
+});
+
+server.listen(8080);
