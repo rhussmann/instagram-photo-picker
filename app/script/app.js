1dc24f2c97cebc0a650f69c4e1d0f2e1972522f4
Ricky Hussmann <ricky.hussmann@gmail.com>
2016-11-21 07:36:10 -0500

    Client-side rendering and selection of `/app/posts.json` * Need to supply your own posts.json * Select photos and copy their URLs to the clipboard * Clipboard contains a comma-separated list

---
+++ app/script/app.js
@@ -0,0 +1,31 @@
+$(function () {
+  $.get('/posts.json', function(posts) {
+    $.get('/templates/thumbnailMonolith.html.mustache', function(template) {
+      var ITEMS_PER_ROW = 4;
+      var START_INDEX = 0;
+      var TOTAL_NUM = 200;
+      var model = {
+        rows: []
+      };
+
+      posts = posts.filter((p) => {return p.type === 'image';});
+      for (var i = START_INDEX; i < TOTAL_NUM; i += ITEMS_PER_ROW) {
+        model.rows.push({images: posts.slice(i, i + ITEMS_PER_ROW)});
+      }
+      var html = Mustache.render(template, model);
+      $('#testing').html(html);
+
+      $('.rhpreview').click(function(){
+        $(this).toggleClass('rhselected');
+      });
+    });
+  });
+
+  new Clipboard('.btn', {
+    text: function() {
+      return $('.rhselected').map(function(i, elem) {
+        return $(elem).attr('src');
+      }).toArray().join(',');
+    }
+  });
+});
